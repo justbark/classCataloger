2016f1b8daed3d00f3951981794986034083d674
justbark <justbark89@gmail.com>
2015-08-11 21:42:55 -0500

    classroom book cataloger
    this allows a teacher to catalog books in a classroom and keep track of
    any student that has borrowed a book

---
+++ classCatalogerWPF/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classCatalogerWPF
+{
+    class Book
+    {
+        public string isbn10; //isbn 10 digit number
+        public string isbn13; //isbn 13 digit number
+        public string title; //title of the book
+        public string author; //name of the author
+        public string editor;
+        public string coAuthor;
+        public string date;
+        public string numberOfPages;
+        public List<String> tags = new List<String>();
+        public bool inOut;
+    }
+}
---
+++ classCatalogerWPF/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace classCatalogerWPF
+{
+    
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        AddBookWindow secondForm = new AddBookWindow();// create a new window object
+                                             // a website showed me to put this here.
+        private void addABookButton_Click(object sender, RoutedEventArgs e)
+        {
+            secondForm.Show();//show new window object
+            Book newBook = new Book();// create a new book
+
+            newBook.author = secondForm.authorTextBox.Text;
+            newBook.title = secondForm.titleTextBox.Text;
+            newBook.editor = secondForm.editorTextBox.Text;
+            newBook.date = secondForm.dateTextBox.Text;
+
+            bookList.Add(newBook);
+            this.Close();
+
+        }
+    }
+}
